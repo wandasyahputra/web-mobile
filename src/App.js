011dac5e225716d0c2b3063c606f2c238d7b40fd
wanda <wandasyaputra03@gmail.com>
2019-03-20 15:52:13 +0700

    create styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import CurrencyInput from 'react-currency-input'
+import logo from './assets/img/logo.svg'
 import './App.css'
 
 class App extends Component {
@@ -9,74 +10,119 @@ class App extends Component {
       denomination: [100000, 50000, 20000, 10000, 5000, 1000, 100, 50],
       fraction: [],
       left: 0,
-      value: ''
+      value: 0,
+      error: ''
     }
-    this.handle = this.handle.bind(this)
   }
 
-  handle(e) {
-    console.log(e.key)
-    console.log(e.target.value)
+  /**
+   * fracting value and assign to state
+   */
+  fraction = () => {
     const { denomination } = this.state
-    if (e.key === 'Enter') {
-      let temp = e.target.value
-      temp = temp.replace('Rp', '')
-      temp = temp.replace(/\./g, '')
-      temp = temp.replace(',', '.')
+      let { value } = this.state
       const fraction = []
-      temp = parseFloat(temp)
-      for (let i = 0; i < denomination.length; i += 1) {
-        let count = 0
-        while (temp >= denomination[i]) {
-          temp -= denomination[i]
-          count += 1
+      if (value !== 0) {
+        /**
+         * check how much in every denomination
+         * and push into fraction array
+         */
+        for (let i = 0; i < denomination.length; i += 1) {
+          let count = 0
+          while (value >= denomination[i]) {
+            value -= denomination[i]
+            count += 1
+          }
+          fraction.push(count)
         }
-        fraction.push(count)
+        this.setState({ fraction, left: value })
+      } else {
+        this.setState({
+          error: "value can't be zero",
+          left: 0,
+          fraction: []
+        })
       }
-      console.log(fraction)
-      window.temp = temp
-      this.setState({ fraction, left: temp.toFixed(2), value: e.target.value })
+  }
+
+   /**
+   * process keyUp event
+   * @param e = event of input
+   */
+  handleKeyUp = (e) => {
+    if (e.key === 'Enter') {
+      this.fraction()
     }
   }
+
+  /**
+   * assign value of input to state
+   * @param event = event of input
+   * @param maskedvalue = value with Prefix and thousandSeparator
+   * @param floatvalue = value in float 
+   */
+  handleChange = (event, maskedvalue, floatvalue) => {
+    this.setState({value: floatvalue, error: '' })
+  }
+
+  /**
+   * handle reset button
+   */
+   handleReset = () => {
+     this.setState({
+       value: '',
+       left: 0,
+       fraction: []
+    })
+   }
 
   render() {
     const {
-      fraction, denomination, left, value
+      fraction, denomination, left, value, error
     } = this.state
     return (
       <div className="App">
+        <div className="App-header">
+          <img className="header-logo" src={logo} alt='' />
+        </div>
         <CurrencyInput
+          className="fractionInput"
           decimalSeparator=","
           thousandSeparator="."
           prefix="Rp"
-          onKeyUp={e => this.handle(e)}
+          precision="0"
+          onChangeEvent={this.handleChange}
+          onKeyUp={this.handleKeyUp}
           allowEmpty
           value={value}
         />
         <div>
-          {value && (
-          <div>
-            {value}
-            {' '}
-=
-          </div>
-          )}
+          <button onClick={this.fraction}>
+            Calculate
+          </button>
+          <button onClick={this.handleReset}>
+            Reset
+          </button>
+        </div>
+        <div>
           <table>
-            {fraction.length > 0
-              && fraction.map((item, key) => {
-                if (item > 0) {
-                  return (
-                    <tr>
-                      <td>{item}</td>
-                      <td> x </td>
-                      <td> Rp</td>
-                      <td>{denomination[key]}</td>
-                    </tr>
-                  )
-                }
-                return null
-              })
-            }
+            <tbody>
+              {fraction.length > 0
+                && fraction.map((item, key) => {
+                  if (item > 0) {
+                    return (
+                      <tr key={key.toString()}>
+                        <td>{item}</td>
+                        <td> x </td>
+                        <td> Rp</td>
+                        <td>{denomination[key]}</td>
+                      </tr>
+                    )
+                  }
+                  return null
+                })
+              }
+            </tbody>
           </table>
           {left > 0 && (
           <div>
@@ -86,6 +132,9 @@ class App extends Component {
             &nbsp;
             no available fraction
           </div>
+          )}
+          {error !== '' && (
+            <div> {error} </div>
           )}
         </div>
       </div>
